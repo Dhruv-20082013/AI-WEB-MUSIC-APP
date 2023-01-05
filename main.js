13496c64912b21db9f672e71e5fc849cb4346318
Dhruv-20082013 <85284596+Dhruv-20082013@users.noreply.github.com>
2023-01-05 14:09:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var song = "";
+var song1 = "";
+var leftWristX = 0;
+var leftWristY = 0;
+var rightWristX = 0;
+var rightWristY = 0;
+function preload()
+{
+    song = loadSound("music.mp3");
+    song1= loadSound("music2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(500, 600);
+    canvas.center();
+
+    video = createCapture(VIDEO); 
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 500, 600);
+}
